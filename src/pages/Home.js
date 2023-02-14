388fe2ab991284b691ce767a366df025fb56f21d
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2023-02-14 14:03:11 +0300

    Upcoming Today

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { DiaryContext } from "../Layout";
+import TodoList from "../components/TodoList";
 
 export default function Home({ today }) {
+
+  const { reactiveDiary } = useContext(DiaryContext)
+
   return (
-    <div className="flex flex-col items-center w-full gap-6 text-center lg:gap-32">
-      <section className="space-y-6">
-        <h1>This is your Agenda!</h1>
+    <div className="flex flex-col items-center w-full gap-12 text-center lg:gap-32">
+      <section className="w-full space-y-6 lg:w-2/3">
+        <h1 className="font-bold lg:text-7xl">This is your Agenda!</h1>
         <p className="text-2xl">
           It's also your diary! Speak with your agenda! Write your tasks, todo's
           or whatever you want.
@@ -15,9 +21,9 @@ export default function Home({ today }) {
           </h1>
         </NavLink>
       </section>
-      <div>
-        <h1>Upcoming Today</h1>
-        {/* buraya o günün todo componenti gelecek */}
+      <div className="w-full lg:w-2/3">
+        <h1 className="pb-4 font-bold lg:pb-12 lg:text-7xl">Upcoming Today</h1>
+        <TodoList localTodoList={reactiveDiary[today].todoList} type={"nothing"}/>
       </div>
     </div>
   );
